53e8990196ffcb3da72489d884c8aefe92e92223
Dom <szborzdynski@gmail.com>
2025-04-26 15:28:11 +0200

    cuda results added

--- CUDA/cuda_job1.sh
+++ CUDA/cuda_job1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #BSUB -J cuda_gpu_quick_test
 #BSUB -q gpua100
-#BSUB -W 0:10
+#BSUB -W 4:00
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=1GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "select[gpu80gb]"
 #BSUB -n 4
-#BSUB -o batch_output/gpujob_%J.out
-#BSUB -e batch_output/gpujob_%J.err
+#BSUB -o gpujob_%J.out
+#BSUB -e gpujob_%J.err
 
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
